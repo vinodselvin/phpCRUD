5353bb307e62915b688c53207b7b927877072721
vinod selvin <vinodselvinnadar@gmail.com>
2016-12-25 12:46:42 +0530

    Dynamic folder structure

--- application/controllers/crud_controller.php
+++ application/controllers/crud_controller.php
@@ -5,11 +5,14 @@ class Crud_controller extends CI_Controller {
 	
 	public function index()
 	{       
-                $data['title'] = 'any titlw';
-                $data['css'] = baseurl('');
-                $this->load->view('static/header');
+                $data['title'] = 'any title';
+                $css[] = base_url('/assets/bootstrap/css/bootstrap.min.css');
+                $js[] = base_url('/assets/bootstrap/js/bootstrap.min.css');
+                $data['css'] = $css;
+                $data['js'] = $js;
+                $this->load->view('static/header', $data);
 		$this->load->view('crud_view');
-                $this->load->view('static/footer');
+                $this->load->view('static/footer', $data);
 	}
 }
 
