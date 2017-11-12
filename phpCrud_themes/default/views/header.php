b7711ec57e230ed36bcafd4760d60b738bd348b2
vinod selvin <vinodselvinnadar@gmail.com>
2017-11-12 22:59:02 +0530

    Added D3.js library and also integrated multiline graph closes #26

--- phpCrud_themes/default/views/header.php
+++ phpCrud_themes/default/views/header.php
@@ -4,5 +4,9 @@
         <title><?php echo $title;?></title>
         <link rel='stylesheet' type='text/css' href="<?php echo base_url( $dir . '/assets/bootstrap/css/bootstrap.min.css'); ?>">
         <link rel='stylesheet' type='text/css' href="<?php echo base_url($dir . '/assets/app.css'); ?>">
+        <?php foreach($css as $key => $value){?>
+    	<link rel='stylesheet' type='text/css' href="<?php echo base_url($dir . $value); ?>">
+    	<?php } ?>
+    	<meta content="utf-8" http-equiv="encoding">
     </head>
     <body ng-app="phpCrud">
